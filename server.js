1f88d1b2623b3409c55dfdc4013a2b0a5f6b5466
Gabriel Luiz <gabrielluiz2121@gmail.com>
2026-05-29 01:16:48 -0300

    fix: define o host 0.0.0.0 para o railway

--- server.js
+++ server.js
@@ -91,6 +91,6 @@ app.post('/api/webhook/openpix', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
